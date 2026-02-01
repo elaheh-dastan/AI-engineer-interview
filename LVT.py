b47ede23fa0e2a26a088cc3d6cf2506444a7153b
Elaheh Dastan <elahe.dstn@gmail.com>
2026-02-01 16:18:22 +0330

    Create LVT.py

---
+++ LVT.py
@@ -0,0 +1,12 @@
+# Task 1: write a function to calculate the similarity (Dot Product) between two vectors.
+# Task 2: Given a query vector and a list of product vectors, find the ID of the top k similar items.
+
+# Example Data
+query_vec = [0.1, 0.5, 0.2]
+
+database = [
+  {"id": 101, "vector": [0.1, 0.5, 0.2]},
+  {"id": 102, "vector": [0.9, 0.1, 0.0]},
+  {"id": 103, "vector": [0.2, 0.4, 0.2]},
+]
+
